fa92ac0bece9eede6d25e6d340f372b0230e2e15
Nahom32 <senaynahom00@gmail.com>
2026-04-13 12:24:23 +0300

    feat: added tests

--- core/helpers.py
+++ core/helpers.py
@@ -3,10 +3,10 @@ from functools import reduce
 def listDifference(list1: list, list2: list) -> list:
     return [list1[i] - list2[i] for i in range(len(list1))]
 
-def norm(array:list) -> float:
+def norm(array:list):
     return reduce(lambda x,y: x+y ,[i**2 for i in array])
 
 
-def calculate_norm_difference(list1: list, list2: list) -> float:
+def calculate_norm_difference(list1: list, list2: list):
     diff_list = listDifference(list1, list2)
-    return round(float(norm(diff_list)), 3)
+    return round(norm(diff_list), 2)
